d6091eca3e9776c5b834c105acef4f2eb6d3cf2b
KatrinMul <mulhabibul@yandex.ru>
2025-11-18 12:58:00 +0300

    Добавлены картинки, оформлен hero для мобильного

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,23 @@
 // https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav__button');
+
+nav.classList.add('nav--closed');
+
+navButton.addEventListener('click', () => {
+  nav.classList.toggle('nav--closed');
+  nav.classList.toggle('nav--opened');
+});
+
+const swiper = new Swiper('.swiper', {
+  modules: [Pagination],
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
