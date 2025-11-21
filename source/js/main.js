b4b2a424dfcd2e0e5f27d353f3fc3655c3f0ee76
KatrinMul <mulhabibul@yandex.ru>
2025-11-21 22:04:48 +0300

    Добавлена стилизация секции туры для мобилы

--- source/js/main.js
+++ source/js/main.js
@@ -13,11 +13,21 @@ navButton.addEventListener('click', () => {
   nav.classList.toggle('nav--opened');
 });
 
-const swiper = new Swiper('.swiper', {
+const swiperHero = new Swiper('.hero__swiper', {
   modules: [Pagination],
   loop: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
+  },
+});
+
+const swiperTours = new Swiper('.tours__swiper', {
+  modules: [Navigation],
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 });
