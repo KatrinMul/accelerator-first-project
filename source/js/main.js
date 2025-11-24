605f482337a990e527fbb648ae67651deff4bc5c
KatrinMul <mulhabibul@yandex.ru>
2025-11-24 21:38:22 +0300

    Добавлена стилизация торов и обучения

--- source/js/main.js
+++ source/js/main.js
@@ -13,21 +13,56 @@ navButton.addEventListener('click', () => {
   nav.classList.toggle('nav--opened');
 });
 
-const swiperHero = new Swiper('.hero__swiper', {
+
+new Swiper('.hero__swiper', {
   modules: [Pagination],
   loop: true,
+  speed: 1500,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
 });
 
-const swiperTours = new Swiper('.tours__swiper', {
+new Swiper('.tours__swiper', {
+  modules: [Navigation],
+  loop: false,
+  speed: 1500,
+  slidesPerView: 1,
+  spaceBetween: 18,
+  slidesPerGroup: 1,
+  navigation: {
+    nextEl: '.tours__swiper-button--next',
+    prevEl: '.tours__swiper-button--prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 18
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+});
+
+new Swiper('.training__swiper', {
   modules: [Navigation],
   slidesPerView: 1,
-  spaceBetween: 30,
+  speed: 1500,
+  spaceBetween: 20,
+  slidesPerGroup: 1,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.training__swiper-button--next',
+    prevEl: '.training__swiper-button--prev',
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 4,
+    }
   },
 });
